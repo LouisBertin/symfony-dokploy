0afc8cadff8466f047c864f12b3f74f0efd33320
Louis Bertin <bertin.louis7@gmail.com>
2025-10-28 18:23:27 +0100

    10 min

--- src/Scheduler/TestTask.php
+++ src/Scheduler/TestTask.php
@@ -20,7 +20,7 @@ class TestTask implements ScheduleProviderInterface
     {
         return (new Schedule())
             ->add(
-                RecurringMessage::every('1 hour', new TestMessage())
+                RecurringMessage::every('10 minutes', new TestMessage())
             );
     }
 }
